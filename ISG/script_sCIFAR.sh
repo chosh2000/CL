f8a088643c8c9844f814beca361179262bbea7da
sanghyun <cho303@purdue.edu>
2020-12-17 20:36:03 -0500

    l

--- ISG/script_sCIFAR.sh
+++ ISG/script_sCIFAR.sh
@@ -2,8 +2,8 @@ OUTDIR=outputs/sCIFAR/$(date +'%b_%d_%Hh_%Mm')
 REPEAT=1
 mkdir -p $OUTDIR
 
-python -u ISG_sCIFAR.py --use_gpu True --repeat $REPEAT --out_dir $OUTDIR  --schedule 60 100 120 --rho 0.5 0.5 0.5 --reglambda 1 --alpha 0.5    | tee ${OUTDIR}/SIM_rho555_reg1.log
-python -u ISG_sCIFAR.py --use_gpu True --repeat $REPEAT --out_dir $OUTDIR  --schedule 60 100 120 --rho 0.5 0.5 0.5 --reglambda 0.1 --alpha 0.5    | tee ${OUTDIR}/SIM_rho555_reg1.log
+python -u ISG_sCIFAR.py --use_gpu True --repeat $REPEAT --out_dir $OUTDIR  --schedule 60 100 --rho 0.5 0.5 0.5 --reglambda 1 --alpha 0.5    | tee ${OUTDIR}/SIM_rho555_reg1.log
+python -u ISG_sCIFAR.py --use_gpu True --repeat $REPEAT --out_dir $OUTDIR  --schedule 60 100 --rho 0.5 0.5 0.5 --reglambda 0.1 --alpha 0.5    | tee ${OUTDIR}/SIM_rho555_reg01.log
 # python -u ISG_sCIFAR.py --use_gpu True --repeat $REPEAT --out_dir $OUTDIR  --schedule 60 100 120 --rho 0.5 0.5 0.5 --reglambda 1 --alpha 0.5    | tee ${OUTDIR}/SIM_rho555_reg1.log
 
 
