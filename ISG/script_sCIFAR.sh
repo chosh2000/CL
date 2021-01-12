cd6054639983436499c73aa612bcc7b99bfb5917
Sanghyun Cho <cho303@purdue.edu>
2021-01-12 15:53:47 -0500

    l

--- ISG/script_sCIFAR.sh
+++ ISG/script_sCIFAR.sh
@@ -37,6 +37,10 @@ mkdir -p $OUTDIR
 # python -u ISG_sCIFAR.py --use_gpu True --repeat $REPEAT --out_dir $OUTDIR  --schedule 50 80  --rho 1 1 0.5 	   --reglambda 1 	--alpha 0    | tee ${OUTDIR}/SIM_rho115_reg1_a00.log
 # python -u ISG_sCIFAR.py --use_gpu True --repeat $REPEAT --out_dir $OUTDIR  --schedule 50 80  --rho 1 0.5 0.5   --reglambda 1 	--alpha 0    | tee ${OUTDIR}/SIM_rho155_reg1_a00.log
 
-python -u ISG_sCIFAR.py --use_gpu True --repeat $REPEAT --out_dir $OUTDIR  --schedule 50 80  --rho 1 0.5 1 	   --reglambda 1 	--random_drop 1    | tee ${OUTDIR}/SIM_rho151_reg1_random.log
-python -u ISG_sCIFAR.py --use_gpu True --repeat $REPEAT --out_dir $OUTDIR  --schedule 50 80  --rho 1 1 0.5 	   --reglambda 1 	--random_drop 1    | tee ${OUTDIR}/SIM_rho115_reg1_random.log
-python -u ISG_sCIFAR.py --use_gpu True --repeat $REPEAT --out_dir $OUTDIR  --schedule 50 80  --rho 1 0.5 0.5   --reglambda 1 	--random_drop 1    | tee ${OUTDIR}/SIM_rho155_reg1_random.log
+# python -u ISG_sCIFAR.py --use_gpu True --repeat $REPEAT --out_dir $OUTDIR  --schedule 50 80  --rho 1 0.5 1 	   --reglambda 1 	--random_drop 1    | tee ${OUTDIR}/SIM_rho151_reg1_random.log
+# python -u ISG_sCIFAR.py --use_gpu True --repeat $REPEAT --out_dir $OUTDIR  --schedule 50 80  --rho 1 1 0.5 	   --reglambda 1 	--random_drop 1    | tee ${OUTDIR}/SIM_rho115_reg1_random.log
+python -u ISG_sCIFAR.py --use_gpu True --repeat $REPEAT --out_dir $OUTDIR  --schedule 50 80  --rho 1 0.7 0.7   --reglambda 1 	--random_drop 1    | tee ${OUTDIR}/SIM_rho177_reg1_random.log
+python -u ISG_sCIFAR.py --use_gpu True --repeat $REPEAT --out_dir $OUTDIR  --schedule 50 80  --rho 1 0.6 0.6   --reglambda 1 	--random_drop 1    | tee ${OUTDIR}/SIM_rho166_reg1_random.log
+python -u ISG_sCIFAR.py --use_gpu True --repeat $REPEAT --out_dir $OUTDIR  --schedule 50 80  --rho 1 0.4 0.4   --reglambda 1 	--random_drop 1    | tee ${OUTDIR}/SIM_rho144_reg1_random.log
+python -u ISG_sCIFAR.py --use_gpu True --repeat $REPEAT --out_dir $OUTDIR  --schedule 50 80  --rho 1 0.3 0.3   --reglambda 1 	--random_drop 1    | tee ${OUTDIR}/SIM_rho133_reg1_random.log
+python -u ISG_sCIFAR.py --use_gpu True --repeat $REPEAT --out_dir $OUTDIR  --schedule 50 80  --rho 1 0.2 0.2   --reglambda 1 	--random_drop 1    | tee ${OUTDIR}/SIM_rho122_reg1_random.log
