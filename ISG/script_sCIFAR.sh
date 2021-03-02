cd1b9ac223573e9e20229febb193d3d1af0f9abc
Sanghyun Cho <cho303@purdue.edu>
2021-03-02 14:04:37 -0500

    l

--- ISG/script_sCIFAR.sh
+++ ISG/script_sCIFAR.sh
@@ -58,8 +58,9 @@ mkdir -p $OUTDIR
 # python -u ISG_sCIFAR.py --use_gpu 1 --repeat $REPEAT --init_model 0 --out_dir $OUTDIR  --schedule 50 80  --dropmethod rho --rho 1 0.4 0.4 --inhib 0.5 --reglambda 1 --alpha 0.0	--beta 0.9	| tee ${OUTDIR}/SIM_rho144_reg1_a0_b09_dropout_inhib05.log
 
 # python -u ISG_sCIFAR.py --use_gpu 1 --repeat $REPEAT --method MAS  --dataset sCIFAR100 --init_model 0 --out_dir $OUTDIR  --schedule 50 80  --apply_SIM 0 --rho 1 1 1 --reglambda 1 | tee ${OUTDIR}/MAS_reg1_dropout.log    
-python -u ISG_sCIFAR.py --use_gpu 1 --repeat $REPEAT --method EWC  --dataset sCIFAR100 --init_model 0 --out_dir $OUTDIR  --schedule 50 80  --apply_SIM 0 --rho 1 1 1 --reglambda 1 | tee ${OUTDIR}/EWC_reg1_dropout.log    
+# python -u ISG_sCIFAR.py --use_gpu 1 --repeat $REPEAT --method EWC  --dataset sCIFAR100 --init_model 0 --out_dir $OUTDIR  --schedule 50 80  --apply_SIM 0 --rho 1 1 1 --reglambda 1 | tee ${OUTDIR}/EWC_reg1_dropout.log    
 # python -u ISG_sCIFAR.py --use_gpu 1 --repeat $REPEAT --method SI   --dataset sCIFAR100 --init_model 0 --out_dir $OUTDIR  --schedule 50 80  --apply_SIM 0 --rho 1 1 1 --reglambda 1 | tee ${OUTDIR}/SI_reg1_dropout.log    
+python -u ISG_sCIFAR.py --use_gpu 1 --repeat $REPEAT --method MAS   --dataset sCIFAR100 --init_model 0 --out_dir $OUTDIR  --schedule 50 80  --apply_SIM 1 --rho 1 0.4 0.4 --reglambda 1 --alpha 0| tee ${OUTDIR}/SIM_rho144_reg1_a0_dropout.log    
 
 
 #Reglambda used for resnet, CIFAR100
