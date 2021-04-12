946815e21652cbc05f0005d7efd3c69c142d6fa9
Sanghyun Cho <cho303@purdue.edu>
2021-04-12 14:47:39 -0400

    pulling

--- ISG/script_pMNIST.sh
+++ ISG/script_pMNIST.sh
@@ -23,6 +23,6 @@ mkdir -p $OUTDIR
 # python -u SIM.py --use_gpu 1 --repeat $REPEAT --out_dir $OUTDIR  --dataset pMNIST  --method MAS  --model_type MLP  --mlp_size 100 --num_task 10 --print_freq 200 --schedule 10 --apply_SIM 1 --random_drop 0 --rho 0.5 0.5  --lr 0.0001 --reglambda 0.01  --alpha 1 --xi 0.01 --revert_head 0    | tee ${OUTDIR}/SIM_rho_5_5_revert0.log
 
 
-python -u SIM.py --use_gpu 1 --repeat $REPEAT --out_dir $OUTDIR  --dataset pMNIST   --method MAS  --model_type MLP  --mlp_size 2000 --num_task 10 --print_freq 200 --schedule 10 10 --apply_SIM 1 --random_drop 1 --rho 0.2 0.2  --lr 0.0001 --reglambda 0.01  --alpha 1 --xi 0.01 --revert_head 0 --finetune_epoch 10   | tee ${OUTDIR}/SIM_rho_22_random.log
+python -u SIM.py --use_gpu 1 --repeat $REPEAT --out_dir $OUTDIR  --dataset pMNIST   --method MAS  --model_type MLP  --mlp_size 2000 --num_task 10 --print_freq 200 --schedule 10 20 --apply_SIM 1 --random_drop 1 --rho 0.2 0.2  --lr 0.0001 --reglambda 0.01  --alpha 1 --xi 0.01 --revert_head 0 --finetune_epoch 20   | tee ${OUTDIR}/SIM_rho_22_random.log
 
 
